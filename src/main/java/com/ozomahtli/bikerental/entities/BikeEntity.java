5e080a1768c4c7a6aaeb1ee2a5d9e3a95993aea9
Your Name <diegod9405@gmail.com>
2024-09-02 19:42:44 -0600

    added db

---
+++ src/main/java/com/ozomahtli/bikerental/entities/BikeEntity.java
@@ -0,0 +1,32 @@
+package com.ozomahtli.bikerental.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="bikes")
+public class BikeEntity {
+    @Id
+    @SequenceGenerator(name = "bikes_generator", sequenceName="bikes_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bikes_generator")
+    private Long id;
+    private String brand;
+    private String image;
+    private String description;
+    private int units;
+    private float review;
+
+}
