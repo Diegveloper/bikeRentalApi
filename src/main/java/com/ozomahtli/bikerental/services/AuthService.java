2f802b377b3198514dda48a7b6b74a94a1e7d94b
Your Name <diegod9405@gmail.com>
2024-09-21 11:47:06 -0600

    added gwt one functionality

--- src/main/java/com/ozomahtli/bikerental/services/AuthService.java
+++ src/main/java/com/ozomahtli/bikerental/services/AuthService.java
@@ -1,4 +1,17 @@
 package com.ozomahtli.bikerental.services;
 
+import com.ozomahtli.bikerental.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+
+    @Autowired
+    private JwtUtils jwt;
+
+    public String getToken(){
+        return jwt.generateToken("diegolin");
+    }
+
 }
